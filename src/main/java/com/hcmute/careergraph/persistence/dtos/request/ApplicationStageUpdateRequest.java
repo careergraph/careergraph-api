77c245f8ffc1ef5aff03122e738e119b27eaefa4
congquynguyen296 <congquynguyen296@gmail.com>
2025-11-12 14:11:52 +0700

    Update UI

--- src/main/java/com/hcmute/careergraph/persistence/dtos/request/ApplicationStageUpdateRequest.java
+++ src/main/java/com/hcmute/careergraph/persistence/dtos/request/ApplicationStageUpdateRequest.java
@@ -3,12 +3,18 @@ package com.hcmute.careergraph.persistence.dtos.request;
 import com.hcmute.careergraph.enums.application.ApplicationStage;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 @Builder
-public record ApplicationStageUpdateRequest(
-        @NotNull(message = "Stage is required")
-        ApplicationStage stage,
-        String note,
-        String changedBy
-) {
+@Getter
+@Setter
+public class ApplicationStageUpdateRequest {
+
+        @NotNull(message = "Stage is not null")
+        private ApplicationStage stage;
+
+        private String note;
+
+        private String changeBy;
 }
