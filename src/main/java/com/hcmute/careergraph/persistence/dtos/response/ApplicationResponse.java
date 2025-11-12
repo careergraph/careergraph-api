fcc41bc9e6f0efd9f5294a8be7b67bd8588e5efc
congquynguyen296 <congquynguyen296@gmail.com>
2025-11-12 19:18:58 +0700

    Update API job

--- src/main/java/com/hcmute/careergraph/persistence/dtos/response/ApplicationResponse.java
+++ src/main/java/com/hcmute/careergraph/persistence/dtos/response/ApplicationResponse.java
@@ -2,23 +2,25 @@ package com.hcmute.careergraph.persistence.dtos.response;
 
 import com.hcmute.careergraph.enums.application.ApplicationStage;
 import lombok.Builder;
-
+import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
 @Builder
-public record ApplicationResponse(
-        String applicationId,
-        String coverLetter,
-        String resumeUrl,
-        Integer rating,
-        String notes,
-        String appliedDate,
-        ApplicationStage currentStage,
-        LocalDateTime stageChangedAt,
-        String stageNote,
-        List<ApplicationStageHistoryResponse> stageHistory,
-        CandidateResponse candidate,
-        JobResponse job
-) {
+public class ApplicationResponse {
+
+    private String applicationId;
+    private String coverLetter;
+    private String resumeUrl;
+    private Integer rating;
+    private String notes;
+    private String appliedDate;
+    private ApplicationStage currentStage;
+    private LocalDateTime stageChangedAt;
+    private String stageNote;
+    private List<ApplicationStageHistoryResponse> stageHistory;
+    private CandidateResponse candidate;
+    private JobResponse job;
+
 }
