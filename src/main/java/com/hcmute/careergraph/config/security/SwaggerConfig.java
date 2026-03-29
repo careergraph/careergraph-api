a6a2fbd3a53a8abbbbc7e9d5fa9b496e4cd4e557
Tcrow06 <tcrow0604@gmail.com>
2026-03-29 16:54:13 +0700

    update fiel main/yml ci cd

--- src/main/java/com/hcmute/careergraph/config/security/SwaggerConfig.java
+++ src/main/java/com/hcmute/careergraph/config/security/SwaggerConfig.java
@@ -7,20 +7,22 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes(
-                                "bearerAuth",
-                                new SecurityScheme()
-                                        .name("bearerAuth")
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")));
-    }
+        @Bean
+        public OpenAPI openAPI() {
+                return new OpenAPI()
+                                .addServersItem(new Server().url("https://thinz.io.vn"))
+                                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                                .components(new Components()
+                                                .addSecuritySchemes(
+                                                                "bearerAuth",
+                                                                new SecurityScheme()
+                                                                                .name("bearerAuth")
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")));
+        }
 }
