9da98bbaf99fac84c69a67f75f938fff49eae075
tcrow06 <tcrow0604@gmail.com>
2026-04-01 15:59:19 +0700

    fix origin

--- src/main/java/com/hcmute/careergraph/config/security/CorsConfig.java
+++ src/main/java/com/hcmute/careergraph/config/security/CorsConfig.java
@@ -17,6 +17,7 @@ public class CorsConfig {
         corsConfiguration.addAllowedOrigin("http://localhost:8000");
         corsConfiguration.addAllowedOrigin("https://thinz.io.vn");
         corsConfiguration.addAllowedOrigin("http://thinz.io.vn");
+        corsConfiguration.addAllowedOrigin("http://api.thinz.io.vn");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.setAllowCredentials(true);
